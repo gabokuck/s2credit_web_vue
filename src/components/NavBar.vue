a7b3b29ca5c9fcb42ae4eccdc2ef2ddf2326a707
Gabriel Rosa <gabokuck1990@gmail.com>
2021-12-13 17:35:42 -0600

    Se empieza a trabajar en el navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,4 +1,35 @@
 <template>
+
+    <div class="bg-gray-800">
+        <div class="flex px-2">
+            <div class="flex h-12 lg:hidden  w-20 bg-gray-300 m-2">
+                <p class="m-auto">1</p>
+            </div>
+            <div class="flex h-12 w-full bg-gray-300 m-2 md:items-center pl-2 lg:h-20 lg:w-60 lg:justify-center">
+                <p class="m-auto md:m-0">2</p>
+            </div>
+            <div class="flex md:hidden h-12 w-20 bg-gray-300 m-2">
+                <p class="m-auto">3</p>
+            </div>
+            <div class="hidden md:hidden h-12 w-20 bg-gray-300 m-2 lg:flex lg:w-full lg:h-20 lg:items-center lg:justify-end lg:px-0">
+                <div class="bg-gray-400 h-full w-1/6 flex">
+                    <p class="m-auto">VENTAJAS</p>
+                </div>
+                <div class="bg-gray-400 h-full w-1/6 flex">
+                    <p class="m-auto">PRODUCTOS</p>
+                </div>
+                <div class="bg-gray-400 h-full w-1/6 flex">
+                    <p class="m-auto">MÓDULOS</p>
+                </div>
+                <div class="bg-gray-400 h-full w-1/6 flex">
+                    <p class="m-auto">CLIENTES</p>
+                </div>
+                <div class="bg-gray-400 h-full w-1/6 flex">
+                    <p class="m-auto">CONTACTO</p>
+                </div>
+            </div>
+        </div>
+    </div>
   
 </template>
 
