a4e04470a3d7ca361c866a5e51c4ad0ac084956b
Gabriel Rosa <gabokuck1990@gmail.com>
2021-12-13 15:02:12 -0600

    Proyecto S2CreditWeb inicializado

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+
+
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import NavBarVue from './components/NavBar.vue';
+</script>
+
+<template>
+  
+  <NavBarVue/>
+
+</template>
+
+<style>
+
+</style>
