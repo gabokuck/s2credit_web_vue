b57f937e3ebb924e2235d53de125cacd89c00b00
Gabriel Rosa <gabokuck1990@gmail.com>
2021-12-14 17:00:42 -0600

    Se añaden los assets del icono y banner de la empresa

---
+++ src/components/Banner.vue
@@ -0,0 +1,23 @@
+<template>
+
+
+  <div class="bg-colorS2AzulObscuro h-screen z-0 flex justify-center items-center p-10">
+
+      <p class="text-white text-2xl font-bold text-center uppercase">S2Credit</p>
+
+  </div>
+ 
+
+</template>
+
+<script>
+export default {
+   
+
+}
+</script>
+
+<style>
+
+
+</style>
